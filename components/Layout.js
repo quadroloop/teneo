df7c4990663a566d0eafcf5170fcf296e25f1f92
Bryce Mercines <bryce.mercines@whitecloak.com>
2022-07-27 00:26:14 +0800

    saving progress on initial content render

---
+++ components/Layout.js
@@ -0,0 +1,32 @@
+import Head from "next/head";
+
+const Layout = (props) => {
+  return (
+    <>
+      <Head>
+        <title>Teneo</title>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css"
+          rel="stylesheet"
+        />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Newsreader:ital,wght@0,600;1,600&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,300;0,500;0,600;0,700;1,300;1,500;1,600;1,700&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,400;1,400&display=swap"
+          rel="stylesheet"
+        />
+        <link href="/css/styles.css" rel="stylesheet" />
+      </Head>
+      {props.children}
+    </>
+  );
+};
+
+export default Layout;
---
+++ components/SectionFold1.js
@@ -0,0 +1,29 @@
+const SectionFold1 = () => {
+  return (
+    <section className="bg-light">
+      <div className="container px-5">
+        <div className="row gx-5 align-items-center justify-content-center justify-content-lg-between">
+          <div className="col-12 col-lg-5">
+            <h2 className="display-4 lh-1 mb-4">
+              Designed with Simplicity in mind.
+            </h2>
+            <p className="lead fw-normal text-muted mb-5 mb-lg-0">
+              RSS3 is a decentralized is the new way of sharing information in
+              the web3 ecosystem.
+            </p>
+          </div>
+          <div className="col-sm-8 col-md-6">
+            <div className="px-5 px-sm-0">
+              <img
+                className="img-fluid rounded-circle"
+                src="https://source.unsplash.com/u8Jn2rzYIps/900x900"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default SectionFold1;
